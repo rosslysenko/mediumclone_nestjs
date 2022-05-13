614eff855390fb76686c352a9ca9233db04437ad
Rostyslav Lysenko <lysenkor@gmail.com>
2022-05-13 17:31:01 +0300

    isolated logic of created new user in service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller()
 export class UserController {
+  constructor(private readonly userServise: UserService) {}
   @Post('users')
   async createUser(): Promise<any> {
-    return 'createUser';
+    return this.userServise.createUser();
   }
 }
