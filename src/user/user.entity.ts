3bcdb19a768975fd920eee303bc48f20b2da3f1a
Rostyslav Lysenko <lysenkor@gmail.com>
2022-05-15 19:36:30 +0300

    add user.entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column({ default: '' })
+  bio: string;
+  @Column({ default: '' })
+  image: string;
+  @Column()
+  password: string;
+}
