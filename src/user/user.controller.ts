c361173218df6754211c296b6f82b3d2a0c4581b
Rostyslav Lysenko <lysenkor@gmail.com>
2022-05-15 20:46:58 +0300

    add  userRepository & save to base

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserEntity } from './user.entity';
 import { UserService } from './user.service';
 
 @Controller()
 export class UserController {
   constructor(private readonly userServise: UserService) {}
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserEntity> {
     return this.userServise.createUser(createUserDto);
   }
 }
